312faf8c323b17c69401909f5de2ddc572309a63
zhyk <zhykzhykzhyk@gmail.com>
2014-04-19 14:56:40 +0800

    incr ver, welcome msg.

--- src/settings.py
+++ src/settings.py
@@ -30,7 +30,7 @@ else:
 if sys.platform.startswith('linux'):
     UPDATE_URL += 'src/'
 
-VERSION = 'THBATTLE V1.0b incr 195 pre2'
+VERSION = 'THBATTLE V1.0b incr 195 pre3'
 
 HALL_NOTICE_URL = 'http://www.thbattle.net/notice.txt'
 
